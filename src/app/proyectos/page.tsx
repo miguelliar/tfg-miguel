b1845b1b957fe2911abf4b16c42d6045c2bff554
uo270927 <uo270927@uniovi.es>
2024-04-30 20:51:55 +0200

    add: header navigation and "proyecto" page

---
+++ src/app/proyectos/page.tsx
@@ -0,0 +1,27 @@
+export default function Page() {
+    return (<main>
+        <h1>Proyectos</h1>
+        <table>
+            <thead>
+            <tr>
+                <th>Codigo</th>
+                <th>Investigador Principal</th>
+                <th>Titulo</th>
+                <th>Financiado</th>
+                <th>Inicio</th>
+                <th>Fin</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            </tbody>
+        </table>
+    </main>);
+}
---
+++ src/app/ui/HeaderNavigation.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export function HeaderNavigation(): JSX.Element {
+    return (
+    <header className="flex justify-center">
+      <Link className="mx-5 my-2" href="/">Inicio</Link>
+      <Link className="mx-5 my-2" href="/proyectos">Proyectos</Link>
+    </header>);
+}
