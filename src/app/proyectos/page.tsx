593caf9f9101d8f15f1ba27c1e1defd83c258849
uo270927 <uo270927@uniovi.es>
2024-05-02 20:15:14 +0200

    add: db connection and data processing

--- src/app/proyectos/page.tsx
+++ src/app/proyectos/page.tsx
@@ -1,4 +1,16 @@
-export default function Page() {
+import { fetchProjectData } from '@/app/db/db';
+
+export default async function Page() {
+    //firstAttemptFunction().then(res => console.log(res.rows)); //conexión de BD
+    const data = await fetchProjectData();
+    const body = (<>{ data.map((row) => (<tr key={row.codigo}>
+        <td>{row.codigo}</td>
+        <td>{row.ip}</td>
+        <td>{row.titulo}</td>
+        <td>{row.financiado}</td>
+        <td>{JSON.stringify(row.inicio)}</td>
+        <td>{JSON.stringify(row.fin)}</td>
+    </tr>)) } </>);
     return (<main>
         <h1>Proyectos</h1>
         <table>
@@ -13,14 +25,7 @@ export default function Page() {
             </tr>
             </thead>
             <tbody>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
+            {body}
             </tbody>
         </table>
     </main>);
