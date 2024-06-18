1138b7d43422508b1e32c0b49506ae3061fd1591
uo270927 <uo270927@uniovi.es>
2024-06-18 19:49:17 +0200

    fix: use of proyectos/crear/constants and remove unused import

--- src/app/investigadores/crear/page.tsx
+++ src/app/investigadores/crear/page.tsx
@@ -2,7 +2,6 @@
 import { CreateForm, CreateFormParams } from "@/app/ui/form/CreateForm";
 import { createInvestigador } from "../../../../db/db";
 import { fields } from './constants.json';
-import './styles.css';
 
 export default async function Page() {
     const formParams : CreateFormParams = {
--- src/app/proyectos/crear/page.tsx
+++ src/app/proyectos/crear/page.tsx
@@ -1,53 +1,12 @@
 'use server';
 import { CreateForm, CreateFormParams } from "@/app/ui/form/CreateForm";
 import { createProyecto } from "../../../../db/db";
+import { fields } from './constants.json';
 
 export default async function Page() {
     const formParams : CreateFormParams = {
         createCallback: createProyecto,
-        fields: [
-        {
-            id: "codigo",
-            label: "Codigo",
-            type: "text",
-            required: true,
-            placeholder: "Introduce el codigo para el proyecto..."
-        },
-        {
-            id: "ip",
-            label: "Investigador Principal",
-            type: "text",
-            required: true,
-            placeholder: "Nombre del investigador principal..."
-        },
-        {
-            id: "titulo",
-            label: "Titulo",
-            type: "text",
-            required: true,
-            placeholder: "Titulo del proyecto..."
-        },
-        {
-            id: "financiado",
-            label: "Entidad financiadora",
-            type: "text",
-            required: true,
-            placeholder: "Entidad financiadora..."
-        },
-        {
-            id: "inicio",
-            label: "Fecha de inicio del proyecto",
-            type: "date",
-            required: true,
-            placeholder: "Introduce la fecha de inicio..."
-        },
-        {
-            id: "fin",
-            label: "Fecha de finalización del proyecto (opcional)",
-            type: "date",
-            required: true,
-            placeholder: "Introduce la fecha de fin..."
-        },],
+        fields,
         submitText: "Crear proyecto"
     };
     
