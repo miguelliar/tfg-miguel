85e4e943e7f423b2cc683b80b1227f20370a1710
miguelliar <uo270927@uniovi.es>
2025-01-26 11:45:38 +0100

    fix: add param validation in create and update

--- db/tables/proyecto.ts
+++ db/tables/proyecto.ts
@@ -73,8 +73,25 @@ export const fetchDistinctProyectosByInvestigadores = async (
   }
 }
 
+const validateInputParameter = (proyecto: ProyectoType) => {
+  const { codigo, ip, titulo, financiado, inicio, fin } = proyecto
+
+  if (!codigo.trim()) return "Codigo cannot be empty"
+  if (!ip.trim()) return "Investigador principal cannot be empty"
+  if (!titulo.trim()) return "Titulo cannot be empty"
+  if (!financiado.trim()) return "Financiado cannot be empty"
+  if (!inicio) return "Inicio cannot be empty"
+  if (fin && fin?.valueOf() < inicio.valueOf())
+    return "Fin date must be after inicio date"
+}
+
 export async function createProyectoItem(proyecto: ProyectoType) {
   const { codigo, ip, coip, titulo, financiado, inicio, fin } = proyecto
+
+  const errorMessage = validateInputParameter(proyecto)
+  if (errorMessage) {
+    throw new Error(errorMessage)
+  }
 
   try {
     await pool.query(proyectoConfig.Create, [
@@ -84,7 +101,7 @@ export async function createProyectoItem(proyecto: ProyectoType) {
       titulo,
       financiado,
       inicio,
-      fin ?? "NULL",
+      fin || null,
     ])
   } catch (error) {
     console.error(proyectoConfig.error.Inserting, error)
@@ -94,6 +111,18 @@ export async function createProyectoItem(proyecto: ProyectoType) {
 export async function updateProyectoItem(proyecto: ProyectoType) {
   const { codigo, ip, coip, titulo, financiado, inicio, fin } = proyecto
 
+  const errorMessage = validateInputParameter(proyecto)
+  if (errorMessage) {
+    throw new Error(errorMessage)
+  }
+
+  const proyectoOriginal = await fetchProyectoByCode(codigo)
+  if (!proyectoOriginal) {
+    throw new Error(
+      "There doesn't exist any error with that codigo that can be updated"
+    )
+  }
+
   try {
     await pool.query(proyectoConfig.Update, [
       ip,
@@ -101,7 +130,7 @@ export async function updateProyectoItem(proyecto: ProyectoType) {
       titulo,
       financiado,
       inicio,
-      fin,
+      fin || null,
       codigo,
     ])
   } catch (error) {
